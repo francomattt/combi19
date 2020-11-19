07c2348923e99b3fcd4a7a5f1d1558c58a15928a
francomattt <franco.pirondo@gmail.com>
2020-11-19 12:35:11 -0300

    Correccion de las rutas de insumos y el formulario de carga

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :insumos
   resources :states
   resources :travels
   resources :combis
@@ -14,4 +15,5 @@ Rails.application.routes.draw do
   get 'ways/destroy/:id', to: 'ways#destroy', as: 'destroy_way' 
   get 'combis/destroy/:id', to: 'combis#destroy', as: 'destroy_combi'
   get 'travels/destroy/:id', to: 'travels#destroy', as: 'destroy_travel'
+  get 'insumos/destroy/:id', to: 'insumos#destroy', as: 'destroy_insumo'
 end
