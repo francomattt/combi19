18d134b4832e6a9d4aa2ca0b74a04e22aee91f0f
tomiwachlin <tomaswachlin@alu.ing.unlp.edu.ar>
2020-12-02 15:54:42 -0300

    Se modifico el formulario para agregar choferes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :dni, :birth_date, :suscribed, :role])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :dni, :birth_date, :suscribed, :role, :phone_number])
     
     end
 
